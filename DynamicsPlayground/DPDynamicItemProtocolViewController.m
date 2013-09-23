0915b7fe9005d449f8baca3b5185ed8c6647531a
rpretto <rpretto@h-art.it>
2013-09-23 11:39:30 +0200

    Add stun implementation for new section

---
+++ DynamicsPlayground/DPDynamicItemProtocolViewController.m
@@ -0,0 +1,38 @@
+//
+//  DPDynamicItemViewController.m
+//  DynamicsPlayground
+//
+//  Created by Renzo Pretto on 9/23/13.
+//  Copyright (c) 2013 Renzo Pretto. All rights reserved.
+//
+
+#import "DPDynamicItemProtocolViewController.h"
+
+@interface DPDynamicItemProtocolViewController ()
+
+@end
+
+@implementation DPDynamicItemProtocolViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
