4337ad2f451bd77ec18a4576848d89c2e2307c14
rgpretto <toradol>
2013-09-24 08:17:29 +0200

    DPDynamicItemViewController: add custom properties

--- DynamicsPlayground/DPDynamicItemViewController.m
+++ DynamicsPlayground/DPDynamicItemViewController.m
@@ -8,6 +8,8 @@
 
 #import "DPDynamicItemViewController.h"
 
+#import <QuartzCore/QuartzCore.h>
+
 @interface DPDynamicItemViewController () <UIDynamicAnimatorDelegate>
 
 @property (strong, nonatomic) UIDynamicAnimator *animator;
@@ -35,6 +37,30 @@
     self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
     self.animator.delegate = self;
     
+    /*
+     We want to show collisions between views and boundaries with different elasticities,
+     we thus associate the two views to gravity and collision behaviors.
+     We will only change the restitution parameter for one of these views.
+     */
+    UIGravityBehavior* gravityBeahvior = [[UIGravityBehavior alloc] initWithItems:@[self.greenView, self.redView]];
+    UICollisionBehavior* collisionBehavior = [[UICollisionBehavior alloc] initWithItems:@[self.greenView, self.redView]];
+    collisionBehavior.translatesReferenceBoundsIntoBoundary = YES;
+    
+    /*
+     A dynamic item behavior gives access to low-level properties of an item in
+     Dynamics, here we change restitution on collisions only for redView,
+     and keep square1 with its default value.
+     */
+    UIDynamicItemBehavior* propertiesBehavior = [[UIDynamicItemBehavior alloc] initWithItems:@[self.redView]];
+    propertiesBehavior.elasticity = 0.5;
+    [propertiesBehavior addAngularVelocity:22.0 forItem:self.redView];
+    
+//    self.redView.layer.cornerRadius = CGRectGetWidth(self.redView.frame) / 2.0f;
+//    self.redView.layer.masksToBounds = YES;
+    
+    [self.animator addBehavior:propertiesBehavior];
+    [self.animator addBehavior:gravityBeahvior];
+    [self.animator addBehavior:collisionBehavior];
     
 }
 
